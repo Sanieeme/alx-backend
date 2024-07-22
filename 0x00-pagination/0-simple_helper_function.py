3f89c0e5a22e77f4f1f33fd4fce82a2d33c87cb5
sanieeme <shoneesani@gmail.com>
2024-07-22 08:58:18 +0000

    update simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -14,5 +14,5 @@ def index_range(page: int, page_size: int) -> tuple:
         tuple
     """
     start = (page - 1) * page_size
-    end = start + page_size
+    end = page * page_size
     return start, end
